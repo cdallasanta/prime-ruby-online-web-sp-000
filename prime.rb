13a3bb27b0781c3116d02b029e7c5a2ddb379b1f
Chris Dalla Santa <chris.dallasanta@gmail.com>
2018-12-20 04:39:26 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,6 @@
 # Add  code here!
 def prime?(num)
-  return false if num < 1
-  return true if num == 1 || num == 2
+  return false if num < 2
 
   range = (2..num).to_a
   !range.any?{|divisor| num % divisor == 0}
